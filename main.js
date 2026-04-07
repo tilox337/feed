be14bb131ac055527b3f22cec902c2afb93bc334
Илья Шестак <ilia1259697@gmail.com>
2026-04-08 00:46:27 +0300

    started

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-function main() {}
+let url = new URL("https://jsonplaceholder.typicode.com/posts");
+let findButEl = document.querySelector("#findBut");
+
+findButEl.addEventListener("click", () => {
+  loadData();
+  findButEl.disabled = true;
+  setTimeout(() => {
+    findButEl.disabled = false;
+  }, 2000);
+}); //сделать debounce как хотел маклаков
+
+async function sleep(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
+}
+
+function loadData() {
+  console.log(1);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector("#list").innerHTML += data;
+    });
+}
