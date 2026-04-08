1f4cd9c8c669d3fd648d7e3244cf32f93335137c
Илья Шестак <ilia1259697@gmail.com>
2026-04-09 01:15:11 +0300

    base

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 let url = new URL("https://jsonplaceholder.typicode.com/posts");
 let findButEl = document.querySelector("#findBut");
+let searchEl = document.querySelector("#search");
+let timeout;
+
+searchEl.addEventListener("input", () => {
+  debounce(() => {
+    document.querySelector("#list").innerHTML = "";
+    loadData(searchEl.value);
+  }, 1000);
+});
 
 findButEl.addEventListener("click", () => {
   loadData();
@@ -7,19 +16,47 @@ findButEl.addEventListener("click", () => {
   setTimeout(() => {
     findButEl.disabled = false;
   }, 2000);
-}); //сделать debounce как хотел маклаков
+});
 
 async function sleep(time) {
   return new Promise((resolve) => {
     setTimeout(resolve, time);
   });
 }
+function debounce(func, delay) {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(func, delay);
+}
 
-function loadData() {
-  console.log(1);
+function loadData(filter = "") {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      document.querySelector("#list").innerHTML += data;
+      data.forEach((element) => {
+        document.querySelector("#list").innerHTML += element.title;
+      });
+      console.log(data);
     });
+}
+
+function loadSimulation() {
+  let i = 0;
+  let inter = setInterval(() => {
+    i++;
+    searchEl.innerHTML = "";
+    for (let j = 0; j < (i % 3) + 1; j++) {
+      searchEl.innerHTML += ".";
+    }
+  }, 100);
+
+  setTimeout(() => {
+    //loadData();
+
+    clearInterval(inter);
+    searchEl.innerHTML = "find";
+  }, 1000);
+
+  //clearInterval(inter);
 }
